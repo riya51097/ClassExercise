11304ac1fadf6a4dd6195ea6169edfa00222b243
riya51097 <riya5dhruvi@gmail.com>
2022-08-04 16:37:09 -0400

    group

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // name!: string;
+  // amount!: number;
+  // newExpense: any;
 
   constructor() {}
+
+  let nameButton = document.querySelector('#name');
+  let amountButton = document.querySelector('#amount');
+  let addbutton = document.querySelector('#addbutton');
+
+  addbutton.addEventListener('click', () => {
+    const addedName = nameButton;
+    const addedAmount = amountButton;
+
+    // if(addedName.length <= 0 || addedAmount <=0 || addedAmount.trim().length <= 0 ){
+    //   return;
+    // }
+    console.log(addedName, addedAmount);
+
+  });
+
+  // addData(): void {
+  //   const newExp = {
+  //     name: this.name,
+  //     amount: this.amount,
+  //   };
+
+  //   this.newExpense.add(newExp);
+  //   console.log(this.name);
+  //   console.log(this.amount);
+  // }
 
 }
