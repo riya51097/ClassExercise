62966c369a42d361a0215566de18f4282f14f856
riya51097 <riya5dhruvi@gmail.com>
2022-08-04 19:48:27 -0400

    done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,42 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  // name!: string;
-  // amount!: number;
-  // newExpense: any;
+
+  name: string;
+  amount: number;
+  expenseName: any = [];
+  expenseAmount: any =[];
+  total =0;
 
   constructor() {}
+onSave() {
+  const addedName = this.name;
+  const addedAmount = this.amount;
+  console.log(addedName, addedAmount);
 
-  let nameButton = document.querySelector('#name');
-  let amountButton = document.querySelector('#amount');
-  let addbutton = document.querySelector('#addbutton');
 
-  addbutton.addEventListener('click', () => {
-    const addedName = nameButton;
-    const addedAmount = amountButton;
-
-    // if(addedName.length <= 0 || addedAmount <=0 || addedAmount.trim().length <= 0 ){
-    //   return;
-    // }
-    console.log(addedName, addedAmount);
-
-  });
-
-  // addData(): void {
-  //   const newExp = {
-  //     name: this.name,
-  //     amount: this.amount,
-  //   };
-
-  //   this.newExpense.add(newExp);
-  //   console.log(this.name);
-  //   console.log(this.amount);
-  // }
-
+ this.expenseName.push(this.name);
+ this.expenseAmount.push(this.amount);
+this.total += Number(this.amount);
+}
 }
